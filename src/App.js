16931a23b11396e0b4d98587d571ec6b4d8d8233
benjamin leong <benjaminlsk@live.com>
2023-05-27 22:59:47 +0800

    add about and skill component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import { useState } from 'react';
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   const [toogleMedia, setToogleMedia] = useState(false);
@@ -11,6 +14,9 @@ function App() {
     <div>
       <Navbar handleToggle={handleToggle} toogleMedia={toogleMedia}/>
       <Home toogleMedia={toogleMedia}/>
+      <About />
+      <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section name='about' className='w-full h-screen bg-[#0f2342]'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full  grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#CB8316]'>
+                        About
+                    </p>
+                </div>
+                <div></div>
+            </div>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='sm:text-right text-4xl font-bold'>
+                    <p>
+                        Hi, I'm Ben! Appreciate your visit to my page. Do look around.
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        I am an aspiring software engineer eager to kick-start my career in the field. 
+                        While I may not have any formal work experience,  I thrive in challenging 
+                        environments and have developed excellent problem-solving and analytical skills 
+                        through independent projects and coursework. I have invested considerable time 
+                        and effort into self-study and personal projects which allowed me to gain 
+                        hands-on experience with various coding languages and technologies. 
+                    </p>
+                    <p className='pt-4'>
+                        I look forward to any posibility of working together in the future!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
